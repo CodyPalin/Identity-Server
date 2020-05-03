9f46ece0c243457030595eeae8e1c7398d556c27
Cody Palin <codypalin@u.boisestate.edu>
2020-05-03 06:46:03 -0600

    added dockerfile to launch servers in containers automatically

---
+++ docker/serversetup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+read -p "Enter Password: " -s pass
+echo $pass | sudo -S docker build -t timeserverimage:1.1 .
+echo "Launching $1 servers..."
+for ((i=1; i<= $1; ++i))
+do 
+    x-terminal-emulator -e "echo $pass | sudo -S docker run --name="idserver${i}" --hostname="idserver${i}" identityimage:1.1"
+done
+sleep 8
+echo "Retrieving IPs..."
+rm -f IPlist.txt
+for ((i=1; i<= $1; ++i))
+do 
+    sudo docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' idserver$i >> IPlist.txt
+done
+echo "IPs saved."
+echo "done"
